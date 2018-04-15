d36b97b3bc54939a55cec4ff6cb58cc8ad44ecaa
Unknown <adriendurot3@hotmail.com>
2018-04-15 20:29:37 +0200

    added flat portion

--- src/AD/PensionBundle/Resources/public/js/pension.js
+++ src/AD/PensionBundle/Resources/public/js/pension.js
@@ -1,5 +1,13 @@
-$('.carousel').carousel({
-  interval: 2000
+$(document).ready( function(){
+	$('.carousel').carousel({
+		  interval: 2000
+	});
+
+	$('.carousel').bind("slide.bs.carousel", function (e) {
+		var id = $(e.relatedTarget).index();
+		$("img.bg-main").removeClass('bg-main');
+		$("#pension-image-tb-"+id).addClass('bg-main');
+	});
 })
 
 function showCarouselImage(id){
@@ -7,3 +15,21 @@ function showCarouselImage(id){
 	$("img.bg-main").removeClass('bg-main');
 	$("#pension-image-tb-"+id).addClass('bg-main');
 }
+
+//function changeThumbnailOnNext(length){
+//	var id = $("#carouselExampleIndicators .carousel-inner div.active").index();
+//	id += 1;
+//	if(id > length-1)
+//		id = 0;
+//	$("img.bg-main").removeClass('bg-main');
+//	$("#pension-image-tb-"+id).addClass('bg-main');
+//}
+//
+//function changeThumbnailOnPrev(length){
+//	var id = $("#carouselExampleIndicators .carousel-inner div.active").index();
+//	id -= 1;
+//	if(id < 0)
+//		id = length-1;
+//	$("img.bg-main").removeClass('bg-main');
+//	$("#pension-image-tb-"+id).addClass('bg-main');
+//}
